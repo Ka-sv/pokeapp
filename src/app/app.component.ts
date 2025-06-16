91a73b660cfee51a1a317d365534fd5749c06fed
Kaique Silva <kaique.guerra013@hotmail.com>
2025-06-16 17:53:18 -0300

    comit iniciar, projeto já conectado com api, já instalamos dependencias e realizado teste realizando uma busca

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from './services/pokemon.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
-  standalone: false,
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  pokemon: any;
+  pokemonName: string = 'pikachu';
+
+  constructor(private pokemonService: PokemonService) {}
+
+  ngOnInit() {
+    this.getPokemonData();
+  }
+
+  getPokemonData() {
+    this.pokemonService.getPokemon(this.pokemonName.toLowerCase()).subscribe({
+      next: (data) => {
+        this.pokemon = data;
+        console.log(data);
+      },
+      error: (err) => {
+        console.error('Erro ao buscar o Pokémon:', err);
+      }
+    });
+  }
 }
